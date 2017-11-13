9f325be1be5972c2dd806d96ecd1b93ed6e02d9a
Tim Shipman <bricklayertim@yahoo.com>
2017-11-13 22:12:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-let driver = {}
+var driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   var driver = {'Sam', [address], '11 Broadway'}
